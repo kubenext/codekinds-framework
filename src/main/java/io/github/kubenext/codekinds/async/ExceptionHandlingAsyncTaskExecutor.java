5db846319a090d2d5a4fec3119b7b9f0a3f95ab0
lisson <li.shangjin@icloud.com>
2020-01-16 05:19:24 +0800

    first commit

---
+++ src/main/java/io/github/kubenext/codekinds/async/ExceptionHandlingAsyncTaskExecutor.java
@@ -0,0 +1,95 @@
+package io.github.kubenext.codekinds.async;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.task.AsyncTaskExecutor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+/**
+ * @author lisson
+ *
+ * ExceptionHandlingAsyncTaskExecutor class.
+ */
+public class ExceptionHandlingAsyncTaskExecutor implements AsyncTaskExecutor, InitializingBean, DisposableBean {
+
+    private final Logger log = LoggerFactory.getLogger(ExceptionHandlingAsyncTaskExecutor.class);
+
+    private static final String EXCEPTION_MESSAGE = "Caught async exception";
+
+    private final AsyncTaskExecutor executor;
+
+    /**
+     * Constructor for ExceptionHandlingAsyncTaskExecutor.
+     *
+     * @param executor a {@link org.springframework.core.task.AsyncTaskExecutor} object.
+     */
+    public ExceptionHandlingAsyncTaskExecutor(AsyncTaskExecutor executor) {
+        this.executor = executor;
+    }
+
+    @Override
+    public void execute(Runnable task) {
+        executor.execute(createWrappedRunnable(task));
+    }
+
+    @Override
+    public void execute(Runnable task, long startTimeout) {
+        executor.execute(createWrappedRunnable(task), startTimeout);
+    }
+
+    @Override
+    public Future<?> submit(Runnable task) {
+        return executor.submit(createWrappedRunnable(task));
+    }
+
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        return executor.submit(createCallable(task));
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (executor instanceof InitializingBean) {
+            InitializingBean bean = (InitializingBean) executor;
+            bean.afterPropertiesSet();
+        }
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        if (executor instanceof DisposableBean) {
+            DisposableBean bean = (DisposableBean) executor;
+            bean.destroy();
+        }
+    }
+
+    private Runnable createWrappedRunnable(final Runnable task) {
+        return () -> {
+            try {
+                task.run();
+            } catch (Exception e) {
+                handle(e);
+            }
+        };
+    }
+
+    private <T> Callable<T> createCallable(final Callable<T> task) {
+        return () -> {
+            try {
+                return task.call();
+            } catch (Exception e) {
+                handle(e);
+                throw e;
+            }
+        };
+    }
+
+    private void handle(Exception e) {
+        log.error(EXCEPTION_MESSAGE, e);
+    }
+
+}
